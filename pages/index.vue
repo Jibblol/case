70bb6d3fe78301ad05bef4329d56e4f17cac866b
vin <vin@novicell.no>
2021-05-27 22:04:58 +0200

    added markup generating for youtube and twitter elements

--- pages/index.vue
+++ pages/index.vue
@@ -7,15 +7,15 @@
       <!-- <input type="submit" v-on:submit.prevent="parseUrl(url)" /> -->
       <b-button @click="parseUrl()">Submit</b-button>
     </form>
-    <div v-if="markup">
+    <div>
       {{ url }}
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue, Getter } from 'nuxt-property-decorator'
-import Smartembed from '~/components/Smartembed.vue'
+import { Component, Vue, Getter } from 'nuxt-property-decorator';
+import Smartembed from '~/components/Smartembed.vue';
 
 @Component({
   components: {
@@ -23,32 +23,58 @@ import Smartembed from '~/components/Smartembed.vue'
   },
 })
 export default class extends Vue {
-  url: string = ''
-  showUrl: boolean = false
+  url: string = '';
+  showUrl: boolean = false;
+  // var markup: any
 
-  youtubeUrl: string = 'https://www.youtube.com/watch?v=9UVdAbFO56k&t=11s'
+  youtubeUrl: string = 'https://www.youtube.com/watch?v=9UVdAbFO56k&t=11s';
   // twitterUrl: string = 'https://twitter.com/politietost?tweetlimit=3'
 
   parseUrl(url: string) {
-    var markup: any 
-
     if (this.url.includes('youtube')) {
-      var youtubeUrl = new URL(this.url)
-      var video_id = youtubeUrl.search.split('v=')[1]
-      var ampersandPosition = video_id.indexOf('&')
+      var youtubeUrl = new URL(this.url);
+      var video_id = youtubeUrl.search.split('v=')[1];
+      var ampersandPosition = video_id.indexOf('&');
       if (ampersandPosition != -1) {
-        video_id = video_id.substring(0, ampersandPosition)
+        video_id = video_id.substring(0, ampersandPosition);
       }
-      console.log(video_id)
+
+      var youtubeElement = document.createElement('amedia-smartembed-iframe');
+      youtubeElement.className = 'am-smartembed am-embed-ratio';
+      youtubeElement.style.paddingTop = '56.25%';
+      youtubeElement.setAttribute(
+        'data-iframeurl',
+        `https://www.youtube.com/embed/${video_id}?start=0`
+      );
+      console.log(youtubeElement);
+    }
+
+    if (this.url.includes('twitter.com')) {
+      var twitterUrl = new URL(this.url);
+      var twitter_id = twitterUrl.pathname.split('/')[1]; // politietost
+      var tweetLimit = twitterUrl.search.split('=')[1];
+
+      // const attrs {
+      //   'data-tweet-limit': '3',
+      //   'data-height': '400',
+      //   'data-chrome': 'nofooter noborders'
+      // }
+
+      var twitterElement = document.createElement('a');
+      twitterElement.className = 'twitter-timeline';
+      twitterElement.setAttribute('data-tweet-limit', `${tweetLimit}`);
+      twitterElement.setAttribute('data-height', '400');
+      twitterElement.setAttribute('data-chrome', 'nofooter noborders');
+      twitterElement.textContent = `Tweets from ${twitter_id}`;
+      console.log(twitterElement);
     } else {
-      alert('Please enter a valid URL')
-      this.url = ''
+      alert('Please enter a valid URL');
+      this.url = '';
     }
     // console.log(testUrl.host)
   }
 }
 </script>
-
 
 <style>
 .container {
